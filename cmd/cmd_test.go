e066ff665ca67569749e91b309eadd98bb6af49d
Jeff Ortel <jortel@redhat.com>
2023-07-07 08:28:45 -0700

    Test focused on RuleSelector.
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- cmd/cmd_test.go
+++ cmd/cmd_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/konveyor/tackle2-addon/command"
 	"github.com/onsi/gomega"
 	"testing"
 )
@@ -9,8 +8,8 @@ import (
 func TestRuleSelector(t *testing.T) {
 	g := gomega.NewGomegaWithT(t)
 	// all clauses
-	rules := Rules{}
-	rules.Labels.Included = []string{
+	selector := RuleSelector{}
+	selector.Included = []string{
 		"p1",
 		"p2",
 		"konveyor.io/source=s1",
@@ -18,76 +17,52 @@ func TestRuleSelector(t *testing.T) {
 		"konveyor.io/target=t1",
 		"konveyor.io/target=t2",
 	}
-	options := command.Options{}
-	err := rules.addSelector(&options)
-	selector :=
+	expected :=
 		"(p1||p2)||((konveyor.io/source=s1||konveyor.io/source=s2)&&(konveyor.io/target=t1||konveyor.io/target=t2))"
-	g.Expect(err).To(gomega.BeNil())
-	g.Expect(len(options)).To(gomega.Equal(2))
-	g.Expect(options[1]).To(gomega.Equal(selector))
+	g.Expect(selector.String()).To(gomega.Equal(expected))
 	// other
-	rules = Rules{}
-	rules.Labels.Included = []string{
+	selector = RuleSelector{}
+	selector.Included = []string{
 		"p1",
 		"p2",
 	}
-	options = command.Options{}
-	err = rules.addSelector(&options)
-	selector = "(p1||p2)"
-	g.Expect(err).To(gomega.BeNil())
-	g.Expect(len(options)).To(gomega.Equal(2))
-	g.Expect(options[1]).To(gomega.Equal(selector))
+	expected = "(p1||p2)"
+	g.Expect(selector.String()).To(gomega.Equal(expected))
 	// sources and targets
-	rules = Rules{}
-	rules.Labels.Included = []string{
+	selector = RuleSelector{}
+	selector.Included = []string{
 		"konveyor.io/source=s1",
 		"konveyor.io/source=s2",
 		"konveyor.io/target=t1",
 		"konveyor.io/target=t2",
 	}
-	options = command.Options{}
-	err = rules.addSelector(&options)
-	selector =
+	expected =
 		"(konveyor.io/source=s1||konveyor.io/source=s2)&&(konveyor.io/target=t1||konveyor.io/target=t2)"
-	g.Expect(err).To(gomega.BeNil())
-	g.Expect(len(options)).To(gomega.Equal(2))
-	g.Expect(options[1]).To(gomega.Equal(selector))
+	g.Expect(selector.String()).To(gomega.Equal(expected))
 	// sources
-	rules = Rules{}
-	rules.Labels.Included = []string{
+	selector = RuleSelector{}
+	selector.Included = []string{
 		"konveyor.io/source=s1",
 		"konveyor.io/source=s2",
 	}
-	options = command.Options{}
-	err = rules.addSelector(&options)
-	selector = "(konveyor.io/source=s1||konveyor.io/source=s2)"
-	g.Expect(err).To(gomega.BeNil())
-	g.Expect(len(options)).To(gomega.Equal(2))
-	g.Expect(options[1]).To(gomega.Equal(selector))
+	expected = "(konveyor.io/source=s1||konveyor.io/source=s2)"
+	g.Expect(selector.String()).To(gomega.Equal(expected))
 	// targets
-	rules = Rules{}
-	rules.Labels.Included = []string{
+	selector = RuleSelector{}
+	selector.Included = []string{
 		"konveyor.io/target=t1",
 		"konveyor.io/target=t2",
 	}
-	options = command.Options{}
-	err = rules.addSelector(&options)
-	selector = "(konveyor.io/target=t1||konveyor.io/target=t2)"
-	g.Expect(err).To(gomega.BeNil())
-	g.Expect(len(options)).To(gomega.Equal(2))
-	g.Expect(options[1]).To(gomega.Equal(selector))
+	expected = "(konveyor.io/target=t1||konveyor.io/target=t2)"
+	g.Expect(selector.String()).To(gomega.Equal(expected))
 	// other and targets
-	rules = Rules{}
-	rules.Labels.Included = []string{
+	selector = RuleSelector{}
+	selector.Included = []string{
 		"p1",
 		"p2",
 		"konveyor.io/target=t1",
 		"konveyor.io/target=t2",
 	}
-	options = command.Options{}
-	err = rules.addSelector(&options)
-	selector = "(p1||p2)||(konveyor.io/target=t1||konveyor.io/target=t2)"
-	g.Expect(err).To(gomega.BeNil())
-	g.Expect(len(options)).To(gomega.Equal(2))
-	g.Expect(options[1]).To(gomega.Equal(selector))
+	expected = "(p1||p2)||(konveyor.io/target=t1||konveyor.io/target=t2)"
+	g.Expect(selector.String()).To(gomega.Equal(expected))
 }
