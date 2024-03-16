248e02658cc1ee9114733cccec2e59f8cdc5935d
Jeff Ortel <jortel@redhat.com>
2024-03-16 08:04:55 -0700

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

---
+++ hack/group.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+host="${HOST:-localhost:8080}"
+state="${1:-Ready}"
+
+curl -X POST ${host}/taskgroups \
+  -H 'Content-Type:application/x-yaml' \
+  -H 'Accept:application/x-yaml' \
+ -d \
+"
+---
+state: ${state}
+addon: analyzer
+components:
+- provider-java
+tasks:
+- application:
+    id: 1
+data:
+  mode:
+    artifact: ""
+    binary: false
+    withDeps: false
+  rules:
+    labels:
+      excluded: []
+      included:
+        - konveyor.io/target=cloud-readiness
+    path: ""
+    tags:
+      excluded: []
+  scope:
+    packages:
+      excluded: []
+      included: []
+    withKnownLibs: false
+  sources: []
+  tagger:
+    enabled: true
+  targets: []
+  verbosity: 0
+"
