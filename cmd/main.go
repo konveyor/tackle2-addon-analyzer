719b54ac62dda741b9afb2dc470489fd31664f3f
Jeff Ortel <jortel@redhat.com>
2024-07-18 04:57:47 -0700

    checkpoint
    Signed-off-by: Jeff Ortel <jortel@redhat.com>

--- cmd/main.go
+++ cmd/main.go
@@ -140,16 +140,16 @@ func main() {
 			} else {
 				return
 			}
-			//
-			// Tags.
-			if d.Tagger.Enabled {
-				if d.Tagger.Source == "" {
-					d.Tagger.Source = Source
-				}
-				err = d.Tagger.Update(application.ID, issues.Tags())
-				if err != nil {
-					return
-				}
+		}
+		//
+		// Tags.
+		if d.Tagger.Enabled {
+			if d.Tagger.Source == "" {
+				d.Tagger.Source = Source
+			}
+			err = d.Tagger.Update(application.ID, issues.Tags())
+			if err != nil {
+				return
 			}
 		}
 
